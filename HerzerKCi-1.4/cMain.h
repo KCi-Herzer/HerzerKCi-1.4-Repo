8f8297ad07baab8e186c64869bff8172ab1f314e
neonhippie_kci <neonhippie.kci@gmail.com>
2022-06-05 21:48:52 -0700

    Added grid @36:18

--- HerzerKCi-1.4/cMain.h
+++ HerzerKCi-1.4/cMain.h
@@ -7,9 +7,12 @@ public:
 	cMain();
 	~cMain();
 
-	wxButton* m_btn1 = nullptr;
+	/*wxButton* m_btn1 = nullptr;
 	wxTextCtrl* m_txt1 = nullptr;
-	wxListBox* m_list1 = nullptr;
+	wxListBox* m_list1 = nullptr;*/
+	int nFieldWidth = 10;
+	int nFieldHeight = 10;
+	wxButton** btn; //Array of pointers to buttons 
 
 	void OnButtonClicked(wxCommandEvent& evt);
 
