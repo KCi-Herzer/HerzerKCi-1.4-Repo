e42ec561b0c4bbe4b414c67689e7244261627be3
neonhippie_kci <neonhippie.kci@gmail.com>
2022-06-05 22:01:25 -0700

    Configured event for the array of buttons  @ 38:13

--- HerzerKCi-1.4/cMain.cpp
+++ HerzerKCi-1.4/cMain.cpp
@@ -1,7 +1,7 @@
 #include "cMain.h"
 
 wxBEGIN_EVENT_TABLE(cMain, wxFrame)
-	EVT_BUTTON(10001, OnButtonClicked)
+	//EVT_BUTTON(10001, OnButtonClicked)
 wxEND_EVENT_TABLE()
 
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "MainFrame", wxPoint(560, 240), wxSize(800, 600))
@@ -19,6 +19,8 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "MainFrame", wxPoint(560, 240), wxSi
 		{
 			btn[y * nFieldWidth + x] = new wxButton(this, 10000 + (y * nFieldWidth + x));
 			grid->Add(btn[y * nFieldWidth + x], 1, wxEXPAND | wxALL);
+
+			btn[y * nFieldWidth + x]->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
 		}
 	}
 	this->SetSizer(grid);
@@ -29,10 +31,15 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "MainFrame", wxPoint(560, 240), wxSi
 cMain::~cMain()
 {
 	//WX Widgets handels the deletion of it's objects
+	delete[] btn;
 }
 
 void cMain::OnButtonClicked(wxCommandEvent& evt)
 {
+	//Get Cord. of button in the field
+	int x = (evt.GetId() - 10000) % nFieldWidth;
+	int y = (evt.GetId() - 10000) / nFieldWidth;
+
 	/*m_list1->AppendString(m_txt1->GetValue());*/
 	evt.Skip(); //You don't need to check parents.
 }
